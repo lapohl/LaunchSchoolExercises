6f43e4a758a90b6fb53b865474ef987a930f7d5e
Lyndsey Pohl <lapohl@umich.edu>
2021-06-12 22:33:02 -0400

    6/12/21 progress

--- GitHub/Portfolio/RSGolf_schedule/golf_todo/scheduler/urls.py
+++ GitHub/Portfolio/RSGolf_schedule/golf_todo/scheduler/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from django.urls import include
 from django.views.generic import RedirectView
 from . import views
+from django.conf.urls import url
 
 urlpatterns = [
   path('', views.home, name='home'),
@@ -10,4 +11,5 @@ urlpatterns = [
   path('log_entry.html', views.log_entry, name='log_entry'),
   path('view_edit_tasks.html', views.edit_task, name='edit_task'),
   path('add_task.html', views.add_task, name='add_task'),
+  url(r'^delete/(?P<id>[0-9]+)/$', views.delete_task, name='delete_task'),
 ]
--- GitHub/Portfolio/RSGolf_schedule/golf_todo/scheduler/views.py
+++ GitHub/Portfolio/RSGolf_schedule/golf_todo/scheduler/views.py
@@ -27,7 +27,20 @@ def edit_task(request):
 
 def add_task(request):
   tasks = Task.objects.all()
+  task_types = Task.objects.order_by().values('type').distinct()
+  areas = Task.objects.order_by().values('area').distinct()
+  context = {
+    'tasks': tasks,
+    'task_types': task_types,
+    'areas': areas,
+  }
+  return render(request, 'add_task.html', context=context)
+
+def delete_task(request, id):
+  task_to_delete = Task.objects.get(id=id)
+  task_to_delete.delete()
+  tasks = Task.objects.all()
   context = {
     'tasks': tasks,
   }
-  return render(request, 'add_task.html', context=context)
+  return render(request, 'view_edit_tasks.html', context=context)
